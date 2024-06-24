a22723b47a0ba16ff46dd3a0818cbfb9fc5bc8fb
zhoupancake <zhouwl6722@gmail.com>
2024-06-24 09:14:43 +0800

    modify the database

--- src/main/resources/db/neusoftpractice_public/supervisor_info.sql
+++ src/main/resources/db/neusoftpractice_public/supervisor_info.sql
@@ -1,17 +1,17 @@
 /*
- Navicat Premium Data Transfer
+ Navicat Premium Dump SQL
 
- Source Server         : localhost_3306
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80035
+ Source Server Version : 80035 (8.0.35)
  Source Host           : localhost:3306
  Source Schema         : neusoftpractice_public
 
  Target Server Type    : MySQL
- Target Server Version : 80035
+ Target Server Version : 80035 (8.0.35)
  File Encoding         : 65001
 
- Date: 08/06/2024 11:03:10
+ Date: 23/06/2024 15:24:12
 */
 
 SET NAMES utf8mb4;
@@ -25,7 +25,7 @@ CREATE TABLE `supervisor_info`  (
   `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `tel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `age` int NOT NULL,
+  `birth_year` int NOT NULL,
   `sex` int NOT NULL,
   `city_id` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
@@ -34,15 +34,17 @@ CREATE TABLE `supervisor_info`  (
 -- ----------------------------
 -- Records of supervisor_info
 -- ----------------------------
-INSERT INTO `supervisor_info` VALUES ('1115615827189559296', '13800138000', 'super1', 23, 1, 1);
-INSERT INTO `supervisor_info` VALUES ('1115615864552419328', '13912345678', 'super2', 19, 0, 2);
-INSERT INTO `supervisor_info` VALUES ('1115615900787011584', '13023456789', 'super3', 25, 1, 3);
-INSERT INTO `supervisor_info` VALUES ('1115615936350515200', '15988888888', 'super4', 37, 0, 4);
-INSERT INTO `supervisor_info` VALUES ('1115615978398412800', '18655555555', 'super5', 23, 1, 5);
-INSERT INTO `supervisor_info` VALUES ('1115616037923975168', '18901234567', 'super6', 25, 0, 6);
-INSERT INTO `supervisor_info` VALUES ('1115616085944561664', '15099999999', 'super7', 29, 1, 7);
-INSERT INTO `supervisor_info` VALUES ('1115616117519282176', '14512345678', 'super8', 31, 0, 8);
-INSERT INTO `supervisor_info` VALUES ('1115616172422721536', '17088888888', 'super9', 51, 1, 9);
-INSERT INTO `supervisor_info` VALUES ('1115616213161996288', '17676543210', 'super10', 43, 0, 10);
+INSERT INTO `supervisor_info` VALUES ('1115615827189559296', '13800138000', 'super1', 2001, 1, 1);
+INSERT INTO `supervisor_info` VALUES ('1115615864552419328', '13912345678', 'super2', 2005, 0, 2);
+INSERT INTO `supervisor_info` VALUES ('1115615900787011584', '13023456789', 'super3', 1999, 1, 3);
+INSERT INTO `supervisor_info` VALUES ('1115615936350515200', '15988888888', 'super4', 1987, 0, 4);
+INSERT INTO `supervisor_info` VALUES ('1115615978398412800', '18655555555', 'super5', 2001, 1, 5);
+INSERT INTO `supervisor_info` VALUES ('1115616037923975168', '18901234567', 'John', 1993, 1, 2);
+INSERT INTO `supervisor_info` VALUES ('1115616085944561664', '15099999999', 'super7', 1995, 1, 7);
+INSERT INTO `supervisor_info` VALUES ('1115616117519282176', '14512345678', 'super8', 1993, 0, 8);
+INSERT INTO `supervisor_info` VALUES ('1115616172422721536', '17088888888', 'super9', 1993, 1, 2);
+INSERT INTO `supervisor_info` VALUES ('1118919022288457728', '13866810017', 'John', 1993, 1, 2);
+INSERT INTO `supervisor_info` VALUES ('1118919141389914112', '13866810017', 'John', 1993, 1, 2);
+INSERT INTO `supervisor_info` VALUES ('1118920660671680512', '13866810027', 'John', 1993, 1, 2);
 
 SET FOREIGN_KEY_CHECKS = 1;
